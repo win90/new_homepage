f1c52f2cb1f3b2ff9f5fb3041bbca9dc71132335
Win Naing <wintunnaing55@gmail.com>
2019-12-01 16:37:18 -0800

    adding form for contact

--- views.py
+++ views.py
@@ -1,6 +1,12 @@
 import requests
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
+from django import forms
+# from .model import Contact
+
+
+
 
 # def index(request):
 #     # This is similar to ones we have done before. Instead of keeping
@@ -13,6 +19,17 @@ from django.shortcuts import render
 #         <a href="/contact-me">Contact me</a> <br />
         
 #     ''')
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField(required=False)
+    phone = forms.CharField(max_length=100)
+    Message = forms.CharField(widget=forms.Textarea)
+
+    
+    
+
 
 
 
@@ -39,9 +56,33 @@ def projects(request):
     return render(request,'projects.html',context)
 
 def contact(request):
-    context = {
+    context = {}
+    if request.method =='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
 
-    }
+
+        if '@' not in email:
+            context['error_message'] = 'bad email'
+            return render(request, 'add_sitter.html', context)
+
+        if len(phone) < 9:
+            context['error_message'] = 'phone number is too short'
+            return render(request, 'add_sitter.html', context)
+
+        if '-' not in phone:
+            context['error_message'] = 'phone number is weird'
+            return render(request, 'add_sitter.html', context)
+
+        Contact.objects.create(
+            name=name,
+            email=email,
+            message=message,
+            phone=phone,
+        )
+
+        
     return render(request,'contact.html',context)
 
 
